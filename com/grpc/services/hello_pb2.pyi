a19ef6fec3995571342f156eedeb63c823f220fe
Oscar Diaz <oscargdi@hotmail.com>
2023-08-16 19:51:21 +0000

    add protos generated files

---
+++ com/grpc/services/hello_pb2.pyi
@@ -0,0 +1,5 @@
+from com.grpc.messages import hello_pb2 as _hello_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
